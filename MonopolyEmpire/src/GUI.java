0d81905166613a27d15d87287d33c4a464cb3f9f
it14149 <it14149@uom.edu.gr>
2015-05-28 23:40:15 +0300

    Περιορισμένη λειτουργικότητα όταν πατας στα διαθέσιμα κουμπιά της αρχικής οθόνης

--- MonopolyEmpire/src/GUI.java
+++ MonopolyEmpire/src/GUI.java
@@ -1,8 +1,12 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.WindowEvent;
 import java.awt.image.*;
 import java.io.*;
+import java.util.ArrayList;
 
 import javax.imageio.*;
 import javax.swing.*;
@@ -27,6 +31,32 @@ public class GUI {
 		instructions = new  JButton(new ImageIcon("instructions.png"));
 		highscore = new  JButton(new ImageIcon("high.png"));
 		exit = new  JButton(new ImageIcon("exit.png"));
+		
+		
+		//Add Action Listeners for the 4 available buttons when you open the game
+		newGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                newGameActionPerformed();
+            }
+        });
+		
+		instructions.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+            	instructionsActionPerformed();
+            }
+        });
+		
+		highscore.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+            	highscoreActionPerformed();
+            }
+        });
+		
+		exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+            	exitActionPerformed();
+            }
+        });
 		
 		mainPanel.add(newGame);
 		mainPanel.add(instructions);	
@@ -55,6 +85,81 @@ public class GUI {
 		
 	}
 	
+	private void newGameActionPerformed(){
+		
+		//Not implemented yet
+	}
+	
+	private void instructionsActionPerformed(){
+		//Incomplete. Need to fix it so that the instructions appear properly on screen. Arrows should be located near the bottom middle. 
+		//Left/Right will cycle through the available pages.
+		
+		JDialog instructionsDialog;
+		JPanel instructionsPanel;
+		JLayeredPane instructionsArrowsPanel;
+		JButton leftArrow, rightArrow;
+		int pagePointer;
+		ArrayList<ImageIcon> instructionsImageList = new ArrayList<ImageIcon>();  	
+		
+		
+		instructionsDialog = new JDialog();
+		instructionsPanel = new JPanel();
+		
+		instructionsArrowsPanel = new JLayeredPane();
+		instructionsArrowsPanel.setLayout(new GridLayout(1,2));
+		leftArrow = new JButton("Prev");
+		rightArrow = new JButton("Next");
+		instructionsArrowsPanel.add(leftArrow);
+		instructionsArrowsPanel.add(rightArrow);
+		
+		ImageIcon instructionsImage1 = new ImageIcon("Images/Screenshot_1.jpg");
+		
+		instructionsImageList.add(instructionsImage1);
+		ImageIcon instructionsImage2 = new ImageIcon("Images/Screenshot_2.jpg");
+		instructionsImageList.add(instructionsImage2);
+		ImageIcon instructionsImage3 = new ImageIcon("Images/Screenshot_3.jpg");
+		instructionsImageList.add(instructionsImage3);
+		ImageIcon instructionsImage4 = new ImageIcon("Images/Screenshot_4.jpg");
+		instructionsImageList.add(instructionsImage4);
+		ImageIcon instructionsImage5 = new ImageIcon("Images/Screenshot_5.jpg");
+		instructionsImageList.add(instructionsImage5);
+		
+		pagePointer = 0; //Initialize on first page
+		instructionsPanel.add(new JLabel(instructionsImageList.get(pagePointer)));			
+		
+		instructionsDialog.setLayout(new BorderLayout());
+		instructionsDialog.add(instructionsPanel, BorderLayout.CENTER);
+		
+		
+		instructionsArrowsPanel.setBounds(360, 370, 150, 100);
+		instructionsArrowsPanel.setOpaque(true);
+		instructionsDialog.add(instructionsArrowsPanel, BorderLayout.CENTER);
+		instructionsArrowsPanel.setBounds(0, 0, 850, 628);
+		
+		instructionsDialog.pack();
+		instructionsDialog.setTitle("Instructions");
+		instructionsDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		instructionsDialog.setVisible(true);
+		instructionsDialog.setPreferredSize(new Dimension(1120, 790));
+
+		//instructionsDialog.setResizable(false);
+		
+	}
+
+	private void highscoreActionPerformed(){
+	
+		JOptionPane.showMessageDialog(frame, "This feature is currently disabled");
+		
+		
+	}
+
+	private void exitActionPerformed(){
+		
+		int flag = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit?", "Exit", JOptionPane.YES_NO_OPTION);
+		if (flag == JOptionPane.YES_OPTION) frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));    	
+		
+	
+	}
 	/*public class ImagePanel extends JPanel{
 
 	    private BufferedImage image;
@@ -74,6 +179,8 @@ public class GUI {
 	    }
 
 	}*/
+
+	
 		
 }
 
