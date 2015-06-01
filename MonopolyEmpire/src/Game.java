331f05e935f82c9567698b937fe5785c800cc700
Kiki <it1458@uom.edu.gr>
2015-06-01 22:18:07 +0300

    Method actOnPosition from Player to Game..

--- MonopolyEmpire/src/Game.java
+++ MonopolyEmpire/src/Game.java
@@ -6,10 +6,46 @@ public class Game {
 	private ArrayList<Player> players;
 	private ArrayList<Card> cards;
 	private ArrayList<Space> spaces;
+	private Go go;
 	
 	
 	public Game() {
+		go = new Go();
+	}
+	
+	public void playerActOnPosition(Player player) {
+		int position = player.getPosition();
+		int previousPosition = player.getPreviousPosition();
 		
+		if(position == 0) {//Go
+			go.passGo(player);
+		} else if(position == 1 || position == 3 || position == 5 || position == 7 || 
+				position == 8 || position == 10 || position == 11 || position == 13 ||
+				position ==14 || position == 16 || position == 17 || position == 19 ||
+				position == 20 || position == 22 || position == 23 || position == 25 ||
+				position == 26 || position == 28 || position == 29 || position == 31 ||
+				position == 33 || position == 35) {//Brand
+			
+		} else if(position == 12 || position == 30) {//Utility
+			
+		} else if(position == 2 || position == 34) {//TowerTax
+			
+		} else if(position == 4 || position == 25) {//Empire
+			
+		} else if(position == 6 || position == 15 || position == 21 || position == 32) {//Chance
+			
+		} else if(position == 9) {//JustVisiting
+			
+		} else if(position == 18) {//FreeParking
+			
+		} else if(position == 27) {//GoToJail
+			
+		}
+		
+		// If the player passed GO without standing on GO.
+		if( (previousPosition<=35 && previousPosition>= 25) && (position>=1 && position<=12) )
+			go.passGo(player);
+		previousPosition = position;
 	}
 	
 
--- MonopolyEmpire/src/Player.java
+++ MonopolyEmpire/src/Player.java
@@ -9,7 +9,6 @@ public class Player {
 	private int position, previousPosition;
 	private ArrayList<Card> cardsInHand;
 	private boolean inJail;
-	private Go go;
 	
 	public void Player(int aCode, int someMoney, int aPosition) {
 		code = aCode;
@@ -18,40 +17,6 @@ public class Player {
 		position = aPosition;
 		cardsInHand = new ArrayList<Card>();
 		inJail = false;
-		go = new Go();
-	}
-	
-	public void actOnPosition() {
-		
-		if(position == 0) {//Go
-			go.passGo(this);
-		} else if(position == 1 || position == 3 || position == 5 || position == 7 || 
-				position == 8 || position == 10 || position == 11 || position == 13 ||
-				position ==14 || position == 16 || position == 17 || position == 19 ||
-				position == 20 || position == 22 || position == 23 || position == 25 ||
-				position == 26 || position == 28 || position == 29 || position == 31 ||
-				position == 33 || position == 35) {//Brand
-			
-		} else if(position == 12 || position == 30) {//Utility
-			
-		} else if(position == 2 || position == 34) {//TowerTax
-			
-		} else if(position == 4 || position == 25) {//Empire
-			
-		} else if(position == 6 || position == 15 || position == 21 || position == 32) {//Chance
-			
-		} else if(position == 9) {//JustVisiting
-			
-		} else if(position == 18) {//FreeParking
-			
-		} else if(position == 27) {//GoToJail
-			
-		}
-		
-		// If the player passed GO without standing on GO.
-		if( (previousPosition<=35 && previousPosition>= 25) && (position>=1 && position<=12) )
-			go.passGo(this);
-		previousPosition = position;
 	}
 	
 	public void addCardInHand(Card card) {
@@ -62,8 +27,8 @@ public class Player {
 		money += m;
 	}
 	
-	public void setMoney(int m) {
-		money = m;
+	public void decreaseMoney(int m) {
+		money -= m;
 	}
 	
 	public boolean hasMoney(int m) {
@@ -80,21 +45,29 @@ public class Player {
 	public int getCode() {
 		return code;
 	}
-
-	public void setCode(int code) {
-		this.code = code;
+	
+	public int getPreviousPosition() {
+		return previousPosition;
 	}
-
+	
 	public int getPosition() {
 		return position;
 	}
 
+	public int getMoney() {
+		return money;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
 	public void setPosition(int position) {
 		this.position = position;
 	}
 
-	public int getMoney() {
-		return money;
+	public void setPreviousPosition(int position) {
+		previousPosition = position;
 	}
 
 	public Skyscraper getMyScyscraper() {
