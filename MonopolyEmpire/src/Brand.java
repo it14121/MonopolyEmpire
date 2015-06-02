266baef3d81b41c413af3fea154a63a54a8b17e0
it14149 <it14149@uom.edu.gr>
2015-06-02 21:58:01 +0300

    fixed a small typo

--- MonopolyEmpire/src/Brand.java
+++ MonopolyEmpire/src/Brand.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Brand extends Brutility {
 	
-	private ArrayList<Brand> brands;
+	private static ArrayList<Brand> brands;
 	private String color;
 	private Player owner;
 	
